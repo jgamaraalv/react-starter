e95585b0ed4c9e9ad5e97d97166cb913463e1b76
João Guilherme do Amaral Vequiato <guilive13@gmail.com>
2019-07-11 11:45:05 -0300

    image loader

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./App.css";
 
+import logo from "./assets/react-logo.png";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return <img src={logo} alt="Logotipo React" />;
 }
 
 export default App;
